6982305fb8f145969128bfabea34474fa3d41758
Leandro Nishijima <leandro.nishijima@gmail.com>
2019-09-01 18:33:19 -0300

    created more tests in accounts_test.exs and created new module of tests (multimedia_test.exs)

--- test/rumbl/accounts_test.exs
+++ test/rumbl/accounts_test.exs
@@ -49,4 +49,32 @@ defmodule Rumbl.AccountsTest do
       assert [] = Accounts.list_users()
     end
   end
+
+  describe "authenticate_by_username_and_pass/2" do
+    @pass "123456"
+    @bad_pass "badpass"
+
+    @unknow_user_email "unknow_user@user.com"
+
+    setup do
+      {:ok, user: user_fixtures(password: @pass)}
+    end
+
+    test "returns user with correct password", %{user: user} do
+      assert {:ok, auth_user} =
+               Accounts.authenticate_by_username_and_password(user.username, @pass)
+
+      assert auth_user.id == user.id
+    end
+
+    test "returns unauthorized error with invalid password", %{user: user} do
+      assert {:error, :unauthorized} =
+               Accounts.authenticate_by_username_and_password(user.username, @bad_pass)
+    end
+
+    test "returns not found error with no matching user for email" do
+      assert {:error, :not_found} =
+               Accounts.authenticate_by_username_and_password(@unknow_user_email, @pass)
+    end
+  end
 end
